e6c1ac624cadda3ccb67ab395e16cb98f3192fea
Brier Gallihugh <bsgallih12@gmail.com>
2022-05-09 22:38:16 -0400

    Initial commit

---
+++ 05-10-22/05-10-22 NYT.R
@@ -0,0 +1,3 @@
+install.packages("tidytuesdayR")
+library(tidytuesdayR)
+NYTdata <- readr::read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-10/nyt_titles.tsv')
