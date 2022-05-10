1d5decda31088e9fa1f0bd30df50a80e1598c393
Brier Gallihugh <bsgallih12@gmail.com>
2022-05-10 18:53:21 -0400

    New Concept

--- 05-10-22/05-10-22 NYT.R
+++ 05-10-22/05-10-22 NYT.R
@@ -1,3 +1,20 @@
-install.packages("tidytuesdayR")
-library(tidytuesdayR)
-NYTdata <- readr::read_tsv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-05-10/nyt_titles.tsv')
+library(tidyverse)
+nytdata <- read.csv(file = "NYTTidyTuesday.csv")
+nytdata <- nytdata[,3:9]
+nytdata$title <- as.character(nytdata$title)
+nytdata$author <- as.character(nytdata$author)
+nytdata$first_week <- lubridate::ymd(nytdata$first_week)
+str(nytdata)
+
+clean <- nytdata %>% group_by(author) %>% summarize(books = n(),
+                                           author = as.factor(author),
+                                           year = year,
+                                           title = title,
+                                           total_weeks = total_weeks) %>% filter(books == 1) %>% 
+  arrange(desc(total_weeks)) %>% head(10) %>% select(-books)
+
+# Best One Hitter Wonder Novels by Author
+
+clean %>% ggplot(aes(x = total_weeks, y = reorder(title,total_weeks))) + geom_col()
+
+
